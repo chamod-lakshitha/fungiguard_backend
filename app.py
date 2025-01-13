40a7db66fb9e5869c4f336150b8a3454df1dedaa
Chamod De SIlva <chamodlakshitha12@gmail.com>
2025-01-13 23:16:04 +0530

    flask app initialized

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, jsonify
+import joblib
+import tensorflow as tf
+import numpy as np
+
+app = Flask(__name__)
+
+model = joblib.load('models/FungiGuard_model.joblib') 
+scaler = joblib.load('models/minmax_scaler.joblib') 
+
+@app.route('/api/v1/predict', methods=['GET'])
+def predict():
+    array_of_inputs = [1.        , 0.8       , 0.45454545, 1.        , 0.63636364,
+       0.66666667, 0.14285714, 0.04577407, 0.21207041, 0.03669543]
+    # data = request.get_json()
+    # features = np.array(data['features']).reshape(1, -1)
+    # scaled_features = scaler.transform(features)
+    # prediction = model.predict(scaled_features)
+    edibility = round(float(model.predict(np.asarray([array_of_inputs]))[0][0]), 3)
+    print(edibility)
+    return jsonify({
+        "prediction" : edibility
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True)
